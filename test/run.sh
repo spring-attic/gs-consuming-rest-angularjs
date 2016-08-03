b709c3a6f971426897612bb1bda0f6ac86063b65
Dave Syer <dsyer@pivotal.io>
2016-08-03 06:28:26 -0700

    Fix test script to always source sdkman init

--- test/run.sh
+++ test/run.sh
@@ -7,8 +7,8 @@ BO0T_VERSION=1.4.0.RELEASE
 if ! [ -d "$HOME/.sdkman/bin" ]; then
     curl -s get.sdkman.io | bash
     sleep 10
-    [ -s "$HOME/.sdkman/bin/sdkman-init.sh" ] && source "$HOME/.sdkman/bin/sdkman-init.sh"
 fi
+[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ] && source "$HOME/.sdkman/bin/sdkman-init.sh"
 if ! [ -d "$HOME/.sdkman/springboot/$BOOT_VERSION" ]; then
     echo "Y" | sdk install springboot $BOOT_VERSION
     sdk use springboot $BOOT_VERSION
