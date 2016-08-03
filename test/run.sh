d892d528b75487da685f3b64d06fe6ff1c8fc357
Dave Syer <dsyer@pivotal.io>
2016-08-03 06:20:22 -0700

    Update test script to be more cautious

--- test/run.sh
+++ test/run.sh
@@ -1,16 +1,24 @@
+#!/bin/sh
+
 cd $(dirname $0)
 
-curl -s get.sdkman.io | bash
-sleep 10
-[[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"
-echo "Y" | sdk install springboot 1.3.0.RELEASE
-sdk use springboot 1.3.0.RELEASE
+BO0T_VERSION=1.4.0.RELEASE
+
+if ! [ -d "$HOME/.sdkman/bin" ]; then
+    curl -s get.sdkman.io | bash
+    sleep 10
+    [ -s "$HOME/.sdkman/bin/sdkman-init.sh" ] && source "$HOME/.sdkman/bin/sdkman-init.sh"
+fi
+if ! [ -d "$HOME/.sdkman/springboot/$BOOT_VERSION" ]; then
+    echo "Y" | sdk install springboot $BOOT_VERSION
+    sdk use springboot $BOOT_VERSION
+fi
 
 cd ../complete
 spring run app.groovy &
 sleep 20
 curl -s http://localhost:8080 > actual.html
-killall "java"
+kill `jps | grep JarLauncher | cut -d " " -f 1`
 
 echo "Let's look at the actual results: `cat actual.html`"
 echo "And compare it to: `cat ../test/expected.html`"
@@ -18,10 +26,10 @@ echo "And compare it to: `cat ../test/expected.html`"
 if diff -w ../test/expected.html actual.html
     then
         echo SUCCESS
-        let ret=0
+        ret=0
     else
         echo FAIL
-        let ret=255
+        ret=255
         exit $ret
 fi
 rm -rf actual.html
