c56a6ec0fd0a095f1fd48f9e2f89d217e6d7d79c
Dave Syer <dsyer@pivotal.io>
2016-08-03 06:54:37 -0700

    Use correct location of springboot

--- test/run.sh
+++ test/run.sh
@@ -2,21 +2,21 @@
 
 cd $(dirname $0)
 
-BO0T_VERSION=1.4.0.RELEASE
+BOOT_VERSION=1.4.0.RELEASE
 
 if ! [ -d "$HOME/.sdkman/bin" ]; then
     curl -s get.sdkman.io | bash
     sleep 10
 fi
 [ -s "$HOME/.sdkman/bin/sdkman-init.sh" ] && . "$HOME/.sdkman/bin/sdkman-init.sh"
-if ! [ -d "$HOME/.sdkman/springboot/$BOOT_VERSION" ]; then
+if ! [ -d "$HOME/.sdkman/candidates/springboot/$BOOT_VERSION" ]; then
     echo "Y" | sdk install springboot $BOOT_VERSION
 fi
 sdk use springboot $BOOT_VERSION
 
 cd ../complete
-spring run app.groovy &
-sleep 20
+$HOME/.sdkman/candidates/springboot/$BOOT_VERSION/bin/spring run app.groovy &
+sleep 30
 curl -s http://localhost:8080 > actual.html
 kill `jps | grep JarLauncher | cut -d " " -f 1`
 
