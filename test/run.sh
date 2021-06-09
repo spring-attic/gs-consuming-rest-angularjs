f1db689839292cc300f1082f3a05624dbdbb0c8f
Greg L. Turnquist <gturnquist@vmware.com>
2021-06-09 15:28:34 -0500

    Polishing.

--- test/run.sh
+++ test/run.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 cd complete
+
+echo "Launching Spring+AngularJS app in background..."
 /usr/local/bin/spring run app.groovy &
+
 sleep 30
+
+echo "Checking if it started in the allotted time..."
 curl -s http://localhost:8080 > actual.html
-kill `jps | grep JarLauncher | cut -d " " -f 1`
 
 echo "Let's look at the actual results: `cat actual.html`"
 echo "And compare it to: `cat ../test/expected.html`"
